5fce587b531fc4715ba1eed5290153d8ae32618d
ymei2296236 <e2296236@cmaisonneuve.qc.ca>
2023-11-30 21:48:32 -0500

    Soumettre le travail

--- README.md
+++
@@ -1,7 +0,0 @@
-# 582-31F-MA Programmation d'interface Web 2 TP 1
-
-# github
-https://github.com/ymei2296236/interface-web2-tp2.git
-
-# Webdev
-https://e2296236.webdev.cmaisonneuve.qc.ca/tp2-prog-interface-2/
--- requetes/fonctionsDB.php
+++ requetes/fonctionsDB.php
@@ -11,9 +11,6 @@ function connexionDB() {
     define('DB_PASSWORD', 'root');		// Localhost MAC
     // define('DB_PASSWORD', '');			// Localhost Windows
 
-    // define('DB_USER', 'e2296236');           // Webdev   
-    // define('DB_PASSWORD', 'owioZ7vb1n0D0d4uLPw4');			// Webdev
-
     $laConnexion = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
             
     if (!$laConnexion) {
@@ -22,7 +19,6 @@ function connexionDB() {
     }
     
     $db = mysqli_select_db($laConnexion, 'to-do-list'); 	// Localhost
-    // $db = mysqli_select_db($laConnexion, 'e2296236');		// Webdev
 
     if (!$db) {
         die ('La base de données n\'existe pas.');
